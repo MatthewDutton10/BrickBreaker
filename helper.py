bb0b32ecfe002771e4e9b70fe84be4944fdac40c
MatthewDutton10 <Matthew.dutton@sbcglobal.net>
2024-05-14 23:08:22 -0500

    Fixed brick spacing, improved readability

--- helper.py
+++ helper.py
@@ -2,17 +2,19 @@ import pygame as pg
 from pygame import Rect
 
 NUM_COLUMNS = 10
-NUM_ROWS = 5
+NUM_ROWS = 4
+BRICK_HEIGHT = 35
+TEXT_ROW_HEIGHT = 50
 
 def initializeBricks(screen):
         bricks = []
         for i in range(0, NUM_COLUMNS): # 10
             for j in range(0, NUM_ROWS): # 5
-                bricks.append(Rect(pg.Vector2((screen.get_width() / 10) * i, 20 * j), ((screen.get_width() / 10), 25)))
+                bricks.append(Rect(pg.Vector2((screen.get_width() / NUM_COLUMNS) * i, BRICK_HEIGHT * j), ((screen.get_width() / NUM_COLUMNS), BRICK_HEIGHT)))
         return bricks
 
 def resetBall(screen):
-     return screen.get_width() / 2, screen.get_height() * 0.5
+     return screen.get_width() / 2, (screen.get_height() * 0.5) + TEXT_ROW_HEIGHT
 
 def resetPlayer(player, screen):
      return (screen.get_width() / 2) - player.width / 2
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 # Example file showing a circle moving on screen
-import pygame, random
+import pygame as pg
+import random
 from pygame import Rect
 
 from helper import *
 
 # TODO 
-# brick sizing/spacing
 # changes between levels
 # high score
 
@@ -13,42 +13,41 @@ from helper import *
 # DOCS: https://www.pygame.org/docs/
 
 # pygame setup
-pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
+pg.init()
+screen = pg.display.set_mode((1280, 720))
+clock = pg.time.Clock()
 running = True
 dt = 0
 
+FIRST_TEXT_ROW_POS = 200
+HORIZONTAL_CENTER = screen.get_width() / 2
+
 playerHeight = screen.get_height() * 0.80
-player_pos = pygame.Vector2(screen.get_width() / 2, playerHeight)
-brick_pos = pygame.Vector2(screen.get_width() / 2, 20)
+player_pos = pg.Vector2(HORIZONTAL_CENTER, playerHeight)
 
 playerVelocity = 0
 player = Rect(player_pos, (screen.get_width() / 10, 25))
 player.x -= player.width / 2
 
-ballX = screen.get_width() / 2
-ballY = screen.get_height() * 0.5
+INITIAL_BALL_Y = screen.get_height() / 2 + TEXT_ROW_HEIGHT
 
-ballDown = True
-ballRight = True
+ballX = HORIZONTAL_CENTER
+ballY = INITIAL_BALL_Y
 
 playerSpeed = 25
-ballGravity = 350 # 300
-ballMaxHoriSpeed = 500
-ballHoriSpeed = random.randint(20,200)
+ballVerticalSpeed = 350 # 300
+ballHoriSpeed = random.randint(20,175)
 if ballHoriSpeed % 2 == 0:
     ballHoriSpeed *= -1
 ballRadius = 10
 
-windowSize = pygame.display.get_window_size()
+windowSize = pg.display.get_window_size()
 
-pygame.display.set_caption("BrickBreaker")
+pg.display.set_caption("BrickBreaker")
 playerCollideTimeout = 0
 brickCollideTimeout = 0
 
 bricks = initializeBricks(screen)
-        
 
 id=0
 brickID = {}
@@ -72,8 +71,8 @@ lives = NUM_LIVES
 paused = False
 pauseTimer = 0
 
-if pygame.font:
-    font = pygame.font.Font(None, 64)
+if pg.font:
+    font = pg.font.Font(None, 64)
 
 levelComplete = False
 level = 1
@@ -82,20 +81,20 @@ combo = 0
 
 while running:
     # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    # pg.QUIT event means the user clicked X to close your window
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("black")
 
-    keys = pygame.key.get_pressed()
+    keys = pg.key.get_pressed()
 
-    if (keys[pygame.K_ESCAPE] and not inGame):
+    if (keys[pg.K_ESCAPE] and not inGame):
         running = False
         continue
-    elif ((keys[pygame.K_LSHIFT] or keys[pygame.K_RSHIFT])
+    elif ((keys[pg.K_LSHIFT] or keys[pg.K_RSHIFT])
         and inGame
         and pauseTimer == 0):
         paused = not paused
@@ -104,7 +103,7 @@ while running:
     elif pauseTimer > 0:
         pauseTimer -= 1
 
-    if ((keys[pygame.K_TAB] or inGame)
+    if ((keys[pg.K_TAB] or inGame)
          and not paused):
         inGame = True
         levelComplete = False
@@ -117,24 +116,24 @@ while running:
             random.shuffle(colors)
     
         # Moving Left
-        if (keys[pygame.K_a] or keys[pygame.K_LEFT]) and playerVelocity > 0:
+        if (keys[pg.K_a] or keys[pg.K_LEFT]) and playerVelocity > 0:
             playerVelocity = -1 * playerSpeed * dt
-        elif (keys[pygame.K_a] or keys[pygame.K_LEFT]):
+        elif (keys[pg.K_a] or keys[pg.K_LEFT]):
             playerVelocity -= playerSpeed * dt
 
         # Moving right
-        if (keys[pygame.K_d] or keys[pygame.K_RIGHT]) and playerVelocity < 0:
+        if (keys[pg.K_d] or keys[pg.K_RIGHT]) and playerVelocity < 0:
             playerVelocity = playerSpeed * dt
-        elif (keys[pygame.K_d] or keys[pygame.K_RIGHT]):
+        elif (keys[pg.K_d] or keys[pg.K_RIGHT]):
             playerVelocity += playerSpeed * dt
 
         # Left and Right
-        if ((keys[pygame.K_a] or keys[pygame.K_LEFT]) and (keys[pygame.K_d] or keys[pygame.K_RIGHT])):
+        if ((keys[pg.K_a] or keys[pg.K_LEFT]) and (keys[pg.K_d] or keys[pg.K_RIGHT])):
             playerVelocity = 0
 
         # Hard stop
-        if not (keys[pygame.K_a] or keys[pygame.K_d]
-                or keys[pygame.K_LEFT] or keys[pygame.K_RIGHT]):
+        if not (keys[pg.K_a] or keys[pg.K_d]
+                or keys[pg.K_LEFT] or keys[pg.K_RIGHT]):
             playerVelocity = 0
 
         player = player.move(playerVelocity, 0)
@@ -142,7 +141,7 @@ while running:
         player.x = max(player.x, 0)
 
         # Ball movement
-        ballY += ballGravity * dt
+        ballY += ballVerticalSpeed * dt
         ballX += ballHoriSpeed * dt
 
         # Ball bounces off walls & ceiling
@@ -154,7 +153,7 @@ while running:
             ballHoriSpeed *= -1
         if (ballY <= 0 + ballRadius):
             ballY = 0 + ballRadius
-            ballGravity *= -1
+            ballVerticalSpeed *= -1
         elif (ballY >= windowSize[1] - ballRadius):
             lives-=1
             inGame=False
@@ -166,19 +165,19 @@ while running:
             combo = 0
     elif lives == 0:
         gameOverText = font.render("Game over :(", True, "red")
-        textpos = gameOverText.get_rect(centerx=screen.get_width() / 2, y=150)
+        textpos = gameOverText.get_rect(centerx=HORIZONTAL_CENTER, y=FIRST_TEXT_ROW_POS)
         screen.blit(gameOverText, textpos)
         finalScoreText = font.render("Final Score: " + str(score), True, "red")
-        finalScorePos = finalScoreText.get_rect(centerx=screen.get_width() / 2, y=200)
+        finalScorePos = finalScoreText.get_rect(centerx=HORIZONTAL_CENTER, y=FIRST_TEXT_ROW_POS + TEXT_ROW_HEIGHT)
         screen.blit(finalScoreText, finalScorePos)
         newGameText = font.render("Press 'tab' to start a new game", True, "red")
-        newGamePos = newGameText.get_rect(centerx=screen.get_width() / 2, y=250)
+        newGamePos = newGameText.get_rect(centerx=HORIZONTAL_CENTER, y=FIRST_TEXT_ROW_POS + (TEXT_ROW_HEIGHT * 2))
         screen.blit(newGameText, newGamePos)
         quitText = font.render("Press 'Esc' to quit", True, "red")
-        quitTextPos = quitText.get_rect(centerx=screen.get_width() / 2, y=300)
+        quitTextPos = quitText.get_rect(centerx=HORIZONTAL_CENTER, y=FIRST_TEXT_ROW_POS + (TEXT_ROW_HEIGHT * 3))
         screen.blit(quitText, quitTextPos)
 
-    elif pygame.font and not paused:
+    elif pg.font and not paused:
         if levelComplete:
             textLine1 = font.render("Level " + str(level-1) + " Complete!", True, "red")
             textLine2 = font.render("Press 'tab' to start next level", True, "red")
@@ -191,34 +190,34 @@ while running:
         textLine3 = font.render("Press 'Esc' to quit", True, "red")
         shiftText = font.render("Press 'Shift' to pause", True, "red")
 
-        textpos2 = textLine1.get_rect(centerx=screen.get_width() / 2, y=150)
+        textpos2 = textLine1.get_rect(centerx=HORIZONTAL_CENTER, y=FIRST_TEXT_ROW_POS)
         screen.blit(textLine1, textpos2)
-        textpos3 = textLine2.get_rect(centerx=screen.get_width() / 2, y=200)
+        textpos3 = textLine2.get_rect(centerx=HORIZONTAL_CENTER, y=FIRST_TEXT_ROW_POS + TEXT_ROW_HEIGHT)
         screen.blit(textLine2, textpos3)
-        textpos4 = textLine3.get_rect(centerx=screen.get_width() / 2, y=250)
+        textpos4 = textLine3.get_rect(centerx=HORIZONTAL_CENTER, y=FIRST_TEXT_ROW_POS + (TEXT_ROW_HEIGHT * 2))
         screen.blit(textLine3, textpos4)
-        shiftPos = shiftText.get_rect(centerx=screen.get_width() / 2, y=300)
+        shiftPos = shiftText.get_rect(centerx=HORIZONTAL_CENTER, y=FIRST_TEXT_ROW_POS + (TEXT_ROW_HEIGHT * 3))
         screen.blit(shiftText, shiftPos)
-    elif pygame.font and paused:
+    elif pg.font and paused:
         pausedText = font.render("Paused", True, "red")
-        pausedPos = pausedText.get_rect(centerx=screen.get_width() / 2, y=200)
+        pausedPos = pausedText.get_rect(centerx=HORIZONTAL_CENTER, y=FIRST_TEXT_ROW_POS)
         screen.blit(pausedText, pausedPos)
 
 
 
         
-    drawnPlayer = pygame.draw.rect(screen, "green", player, 40) 
-    drawnBall = pygame.draw.circle(screen, "white", pygame.Vector2(ballX, ballY), ballRadius)
+    drawnPlayer = pg.draw.rect(screen, "green", player, 40) 
+    drawnBall = pg.draw.circle(screen, "white", pg.Vector2(ballX, ballY), ballRadius)
 
     drawnBricks = []
     removeBrickIDs = []
     x=0
     for brick in bricks:
         currentBrickID = getBrickID(brick, brickID)
-        drawnBricks.append(pygame.draw.rect(screen, colors[currentBrickID % len(colors)], brick, 40))
-        if (pygame.Rect.colliderect(drawnBricks[x], drawnBall)
+        drawnBricks.append(pg.draw.rect(screen, colors[currentBrickID % len(colors)], brick, 40))
+        if (pg.Rect.colliderect(drawnBricks[x], drawnBall)
             and brickCollideTimeout == 0):
-            ballGravity *= -1
+            ballVerticalSpeed *= -1
             removeBrickIDs.insert(0, currentBrickID)
             combo += scoreMultiplier*MULTIPLIER_VAL
             scoreMultiplier+=1
@@ -242,6 +241,7 @@ while running:
         drawnBricks = []
         bricks = initializeBricks(screen)
         level+=1
+        ballVerticalSpeed = abs(ballVerticalSpeed)
         # ballGravity += (ballGravity*0.05)
         scoreMultiplier=0
         random.shuffle(colors)
@@ -249,21 +249,21 @@ while running:
         ballX, ballY = resetBall(screen)
         for brick in bricks:
             currentBrickID = getBrickID(brick, brickID)
-            drawnBricks.append(pygame.draw.rect(screen, colors[currentBrickID % len(colors)], brick, 40))
+            drawnBricks.append(pg.draw.rect(screen, colors[currentBrickID % len(colors)], brick, 40))
 
 
     if (brickCollideTimeout > 0):
         brickCollideTimeout -= 1
 
-    if (pygame.Rect.colliderect(drawnPlayer, drawnBall) # do ball and player collide
+    if (pg.Rect.colliderect(drawnPlayer, drawnBall) # do ball and player collide
         and ballY <= playerHeight + 5 # ignore collisions with the side of the rectangle with some tolerance
         and playerCollideTimeout == 0): # add timeout to prevent spamming direction changes
         horiDir = 1
         if drawnBall.x <= (drawnPlayer.x + (drawnPlayer.width/2)):
             horiDir = -1
-        ballHoriSpeed = horiDir * abs((drawnBall.x - (drawnPlayer.x + (drawnPlayer.width/2)))/(drawnPlayer.width)) * ballMaxHoriSpeed
+        ballHoriSpeed = horiDir * abs((drawnBall.x - (drawnPlayer.x + (drawnPlayer.width/2)))/(drawnPlayer.width)) * (abs(ballVerticalSpeed) + 50)
         
-        ballGravity *= -1
+        ballVerticalSpeed *= -1
         playerCollideTimeout = 5
         touchedPlayer = True
         score += combo
@@ -278,7 +278,7 @@ while running:
     screen.blit(scoreText, scoreTextpos)
 
     levelText = font.render("Level: " + str(level), True, "red")
-    levelTextPos = levelText.get_rect(centerx=screen.get_width() / 2, y=screen.get_height() - 50)
+    levelTextPos = levelText.get_rect(centerx=HORIZONTAL_CENTER, y=screen.get_height() - 50)
     screen.blit(levelText, levelTextPos)
 
     livesText = font.render("Lives: " + str(lives), True, "red")
@@ -286,11 +286,11 @@ while running:
     screen.blit(livesText, livesTextPos)
 
     # flip() the display to put your work on screen
-    pygame.display.flip()
+    pg.display.flip()
 
     # limits FPS to 60
     # dt is delta time in seconds since last frame, used for framerate-
     # independent physics.
     dt = clock.tick(60) / 1000
 
-pygame.quit()
+pg.quit()
